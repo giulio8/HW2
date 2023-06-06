4cb2fc49c4eb1b53dd4366f11ee2cec3509ca4b6
giulio <giuliosichili@gmail.com>
2023-06-06 15:05:28 +0200

    footer patch

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -1,5 +1,13 @@
 <footer id="my-footer">
-    <p>Developed by <strong>Giulio Sichili</strong></p>
-    <p>Powered by <strong>HTML</strong>, <strong>CSS</strong>, <strong>PHP</strong> and <strong>JS</strong></p>
-    <p>© 2023</p>
+    <div>
+        <p>Developed by Giulio Sichili</p>
+        <p>Powered by HTML, CSS, PHP and JS</p>
+        <p>© 2023</p>
+    </div>
+    <div>
+        <p>Follow me on:</p>
+        <a href><img src="{{ asset('/assets/—Pngtree—white icon_3570420.png')}}" class="social"></a>
+        <a href><img src="{{ asset('/assets/pngfind.com-instagram-png-white-2773517.png') }}" class="social"></a>
+        <a href><img src="{{ asset('/assets/linkedin-icon-18-ffffff-108.png') }}" class="social"></a>
+    </div>
 </footer>
